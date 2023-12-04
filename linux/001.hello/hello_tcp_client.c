e967fc1fbd5f8a61d1b62c80c54d0f4f07335abd
haru.keiko <haru.keiko@gmail.com>
2023-12-05 01:41:05 +0900

    add code to test TCP boundary..

--- linux/001.hello/hello_tcp_client.c
+++ linux/001.hello/hello_tcp_client.c
@@ -9,7 +9,8 @@ int main(int argc, char* argv[]){
     int sock;
     struct sockaddr_in serv_addr;
     char message[BUFSIZE];
-    int str_len;
+    int str_len = 0, read_len = 0;
+    int index = 0;
     
     if (argc > 1) {
         SERVER_IP = argv[1];
@@ -29,10 +30,23 @@ int main(int argc, char* argv[]){
         err_handling("connect() error.. ");
     }
 
+#if 0
+    // original code to read data from server.
     str_len = read(sock, message, sizeof(message)-1);
     if (str_len < 0) {
         err_handling("read() error.. ");
     }
+#else
+    // TCP Socket doesn't have boundary..
+    // server write data 1 time, but client can read data several times.
+    while (read_len = read(sock, &message[index++], 1)) {
+        if (read_len < 0) {
+            err_handling("read() error.. ");
+        }
+        printf("read_len: %d, str_len: %d.. \n", read_len, str_len);
+        str_len += read_len;
+    }
+#endif
 
     printf("Meesage from Server: %s \n", message);
 
